aba7c79adadaf204061ffb8377793796b5d64da1
kazuki <s.kazuki.japan@gmail.com>
2016-06-07 11:42:01 +0000

    devise日本語化

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable # confirmable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         
+  validates :name, presence: true, length: { maximum: 50 }
 end
