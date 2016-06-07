31105aebef80d5929f94e67cf23894525c56fe7b
kazuki <s.kazuki.japan@gmail.com>
2016-06-07 13:53:01 +0000

    ユーザー名なし

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,4 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
-         
-  validates :name, presence: true, length: { maximum: 50 }
 end
