5770a379a9acb9df846a27c2bb51d1d4ccaa67fc
kazuki <s.kazuki.japan@gmail.com>
2016-06-07 12:19:49 +0000

    データベースname追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160606104719) do
+ActiveRecord::Schema.define(version: 20160607121527) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,7 +32,6 @@ ActiveRecord::Schema.define(version: 20160606104719) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name",                   default: "", null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -49,6 +48,7 @@ ActiveRecord::Schema.define(version: 20160606104719) do
     t.string   "unconfirmed_email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name",                   default: "", null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
