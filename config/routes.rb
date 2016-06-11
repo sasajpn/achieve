3a9bb09b2ff874f3438166f1bc9895ecc7e722b1
kazuki <s.kazuki.japan@gmail.com>
2016-06-11 00:55:11 +0000

    routes変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: "users/sessions",
     registrations: "users/registrations",
-    passwords: "users/passwords",
+    # passwords: "users/passwords",
     omniauth_callbacks: "users/omniauth_callbacks"
   }
   resources :users, only: [:index, :show]
