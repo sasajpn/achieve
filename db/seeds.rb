9fdfba26d9a97c2b596fa2109b25dca275f74f1b
kazuki <s.kazuki.japan@gmail.com>
2016-06-13 10:31:47 +0000

    ブログ削除限定

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,17 @@
-3.times do |n|
-    name = Faker::Name.name
-    email = "sample-#{n+1}@sample.com"
-    password = "password"
-    uid = "sample-#{n+1}"
-    user = User.new(name:  name,
-                        email: email,
-                        password: password,
-                        password_confirmation: password,
-                        uid: uid
-                        )
-    user.skip_confirmation!
-    user.save
-    if user.persisted?
-    user.blogs.create(title: "#{n+1}", content: "sample-#{n+1}")
-    end
-end
+# 3.times do |n|
+#     name = Faker::Name.name
+#     email = "sample-#{n+1}@sample.com"
+#     password = "password"
+#     uid = "sample-#{n+1}"
+#     user = User.new(name:  name,
+#                         email: email,
+#                         password: password,
+#                         password_confirmation: password,
+#                         uid: uid
+#                         )
+#     user.skip_confirmation!
+#     user.save
+#     if user.persisted?
+#     user.blogs.create(title: "#{n+1}", content: "sample-#{n+1}")
+#     end
+# end
