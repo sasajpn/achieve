1fd2e3790cf93745b16f99867f65d4101548645e
kazuki <s.kazuki.japan@gmail.com>
2016-06-14 12:03:25 +0000

    エラーページ実装

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,18 @@
-1000.times do |n|
+2.times do |n|
     name = Faker::Name.name
     email = "sample-#{n+1}@sample.com"
     password = "password"
     uid = "sample-#{n+1}"
     user = User.new(name:  name,
-                        email: email,
-                        password: password,
-                        password_confirmation: password,
-                        uid: uid
-                        )
+                    email: email,
+                    password: password,
+                    password_confirmation: password,
+                    uid: uid
+                    )
     user.skip_confirmation!
     user.save
-    if user.persisted?
-    user.blogs.create(title: "#{n+1}", content: "sample-#{n+1}")
+    blog = user.blogs.build(title: "#{n+1}", content: "sample-#{n+1}")
+    if blog.user_id.present?
+        blog.save
     end
 end
