25ad3f2c59838f2288c1725280a34d74c105c3de
kazuki <s.kazuki.japan@gmail.com>
2016-06-21 10:37:20 +0000

    entryfield.controller.setting

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -69,6 +69,6 @@ class CommentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
-      params.require(:comment).permit(:blog_id, :user_id)
+      params.require(:comment).permit(:content, :blog_id, :user_id)
     end
 end
