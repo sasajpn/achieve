ceadac585cbd5c8445c14e30fd3f87a12b533247
kazuki <s.kazuki.japan@gmail.com>
2016-07-04 12:58:05 +0000

    day18テキスト

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,4 +53,9 @@ Rails.application.configure do
 
   #deviseの設定
   # config.action_mailer.default_url_options = { host: 'blooming-ridge-58222.herokuapp.com' }
+  
+  require 'pusher'
+  Pusher.app_id = ENV['PUSHER_APP_ID']
+  Pusher.key = ENV['PUSHER_KEY']
+  Pusher.secret = ENV['PUSHER_SECRET']
 end
