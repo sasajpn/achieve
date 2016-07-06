8e9be6645e82a35d121d784ed11b7843881a7fd7
kazuki <s.kazuki.japan@gmail.com>
2016-07-06 18:29:03 +0000

    day18

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,7 +59,9 @@ Rails.application.configure do
   # Pusher.key = ENV['PUSHER_KEY']
   # Pusher.secret = ENV['PUSHER_SECRET']
   
-  Pusher.app_id = "222452"
-  Pusher.key = "31f8f29da9da9065dbfa"
-  Pusher.secret = "b606bf29221facff149f"
+  pusher_client = Pusher::Client.new(
+  app_id: "222452",
+  key: "31f8f29da9da9065dbfa",
+  secret: "b606bf29221facff149f"
+  );
 end
--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -4,9 +4,15 @@ require 'pusher'
  # Pusher.key = ENV['PUSHER_KEY']
  # Pusher.secret = ENV['PUSHER_SECRET']
  
- Pusher.app_id = "222452"
- Pusher.key = "31f8f29da9da9065dbfa"
- Pusher.secret = "b606bf29221facff149f"
+ # Pusher.app_id = "222452"
+ # Pusher.key = "31f8f29da9da9065dbfa"
+ # Pusher.secret = "b606bf29221facff149f"
+ 
+  pusher_client = Pusher::Client.new(
+  app_id: "222452",
+  key: "31f8f29da9da9065dbfa",
+  secret: "b606bf29221facff149f"
+  );
   
  Pusher.logger = Rails.logger
  Pusher.encrypted = true
