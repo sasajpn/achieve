8537d6d10d4e8cb25686fa3bf6c85b1bde0920d0
kazuki <s.kazuki.japan@gmail.com>
2016-07-07 15:21:36 +0000

    day18課題

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -32,16 +32,18 @@ class MessagesController < ApplicationController
 
   def create
     @message = @conversation.messages.build(message_params)
-    @notification = @message.notifications.build(recipient_id: @message.conversation.recipient_id, sender_id: current_user.id)
+    
     if @message.save
       if @message.user_id == @conversation.sender_id
         Pusher['notifications_'+@message.conversation.recipient_id.to_s].trigger('message', {
           messaging: "メッセージが届いています。:#{@message.body}"
         })
+        @notification = @message.notifications.create(recipient_id: @message.conversation.recipient_id, sender_id: @message.conversation.sender_id)
       else
         Pusher['notifications_'+@message.conversation.sender_id.to_s].trigger('message', {
           messaging: "メッセージが届いています。:#{@message.body}"
         })
+        @notification = @message.notifications.create(recipient_id: @message.conversation.sender_id, sender_id: @message.conversation.recipient_id)
       end
       redirect_to conversation_messages_path(@conversation)
     end
