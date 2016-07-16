795ad8e698d1bfec310ae63cd2364f6d5380f085
kazuki <s.kazuki.japan@gmail.com>
2016-07-16 06:11:32 +0000

    Add for deploy by capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,35 +7,30 @@
   set :deploy_to,     '/var/www/rails/achieve'
   set :linked_dirs,   fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
   set :keep_releases, 3
-
+  set :linked_files, %w{config/database.yml}
+  
   namespace :deploy do
+  
+    # 上記linked_filesで使用するファイルをアップロードするタスク
+    desc 'Upload database.yml'
+    task :upload do
+      on roles(:app) do |host|
+        if test "[ ! -d #{shared_path}/config ]"
+          execute "mkdir -p #{shared_path}/config"
+        end
+        upload!('config/database.yml', "#{shared_path}/config/database.yml")
+      end
+    end
+  
     desc 'Restart application'
     task :restart do
-      invoke 'unicorn:restart'
-    end
-    desc 'Create database'
-    task :db_create do
-      on roles(:db) do |host|
-        with rails_env: fetch(:rails_env) do
-          within current_path do
-            execute :bundle, :exec, :rake, 'db:create'
-          end
-        end
-      end
-    end
-    desc 'Run seed'
-    task :seed do
       on roles(:app) do
-        with rails_env: fetch(:rails_env) do
-          within current_path do
-            execute :bundle, :exec, :rake, 'db:seed'
-          end
-        end
+        invoke 'unicorn:restart'
       end
     end
-    after :publishing, :restart
-    after :restart,    :clear_cache do
-      on roles(:web), in: :groups, limit: 3, wait: 10 do; end
-    end
-    after :finished,   :cleanup
   end
+  
+  # linked_filesで使用するファイルをアップロードするタスクは、deployが行われる前に実行する必要がある
+  before 'deploy:starting', 'deploy:upload'
+  # Capistrano 3.1.0 からデフォルトで deploy:restart タスクが呼ばれなくなったので、ここに以下の1行を書く必要がある
+  after 'deploy:publishing', 'deploy:restart'
