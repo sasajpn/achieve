e42870757f86482ba01b007ea059f34d18cae3cd
kazuki <s.kazuki.japan@gmail.com>
2016-07-16 05:02:06 +0000

    set environment for deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,86 +1,47 @@
-# config valid only for current version of Capistrano
-lock '3.4.0'
-
-# アプリケーション名
-set :application, 'achieve'
-
-# deployするレポジトリ
-set :repo_url, 'git@github.com:sasajpn/achieve.git'
-
-# Default branch is :master
-# deployするブランチ。デフォルトはmasterなのでなくても可。
-set :branch, 'master'
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
-# Default deploy_to directory is /var/www/my_app_name
-# deploy先のディレクトリ
-set :deploy_to, '/var/www/rails/achieve'
-
-# Default value for :scm is :git
-# set :scm, :git
-
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# シンボリックリンクをはるファイル。今回はgemのconfigを使用して、production.ymlを共通化。
-# set :linked_files, fetch(:linked_files, []).push('config/settings/production.yml')
-# set :linked_files, %w{config/database.yml}
-# set :linked_files, %w{ config/secrets.yml }
-
-# Default value for linked_dirs is []
-# シンボリックリンクをはるフォルダ。
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# 保持するバージョンの個数
-set :keep_releases, 5
-
-# rubyのバージョン
-# set :rbenv_type, :user
-set :rbenv_ruby, '2.3.0'
-
-namespace :deploy do
-  desc 'Restart application'
-  task :restart do
-    invoke 'unicorn:restart'
-  end
-  desc 'Create database'
-  task :db_create do
-    on roles(:db) do |host|
-      with rails_env: fetch(:rails_env) do
-        within current_path do
-          execute :bundle, :exec, :rake, 'db:create'
+  # config valid only for current version of Capistrano
+  lock '3.4.0'
+  set :rbenv_ruby,    '2.3.0'
+  set :application,   'kazuki'
+  set :repo_url,      'git@github.com:sasajpn/achieve.git'
+  set :branch,        'master'
+  set :deploy_to,     '/var/www/rails/achieve'
+  set :linked_dirs,   fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+  set :keep_releases, 3
+  # set values for slackistrano deployment notifier
+  # set :slack_webhook,      'https://hooks.slack.com/services/T02B5F7S3/B0ATNSDB6/ERhof0moz0987uLtiXXXXXX'
+  # set :slack_icon_url,     'https://s3-ap-northeast-1.amazonaws.com/tomajax/images/tomato3_small.png'
+  # set :slack_username,     'Tomajax'
+  # set :slack_msg_starting, "#{ENV['USER'] || ENV['USERNAME']} による #{fetch :branch} ブランチの #{fetch :rails_env, 'production'} 環境へのデプロイが始まります。"
+  # set :slack_msg_finished, "#{ENV['USER'] || ENV['USERNAME']} による #{fetch :branch} ブランチの #{fetch :rails_env, 'production'} 環境へのデプロイが成功しました！！"
+  # set :slack_msg_failed,   "#{ENV['USER'] || ENV['USERNAME']} による #{fetch :branch} ブランチの #{fetch :rails_env, 'production'} 環境へのデプロイが失敗しました..."
+  namespace :deploy do
+    desc 'Restart application'
+    task :restart do
+      invoke 'unicorn:restart'
+    end
+    desc 'Create database'
+    task :db_create do
+      on roles(:db) do |host|
+        with rails_env: fetch(:rails_env) do
+          within current_path do
+            execute :bundle, :exec, :rake, 'db:create'
+          end
         end
       end
     end
-  end
-  desc 'Run seed'
-  task :seed do
-    on roles(:app) do
-      with rails_env: fetch(:rails_env) do
-        within current_path do
-          execute :bundle, :exec, :rake, 'db:seed'
+    desc 'Run seed'
+    task :seed do
+      on roles(:app) do
+        with rails_env: fetch(:rails_env) do
+          within current_path do
+            execute :bundle, :exec, :rake, 'db:seed'
+          end
         end
       end
     end
-  end
-  after :publishing, :restart
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+    after :publishing, :restart
+    after :restart,    :clear_cache do
+      on roles(:web), in: :groups, limit: 3, wait: 10 do; end
     end
+    after :finished,   :cleanup
   end
-end
